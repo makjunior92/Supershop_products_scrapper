61595d7bb0e88e49fd7cd7b7a38eedf6efae80d3
makjunior92 <makjunior92@gmail.com>
2018-11-22 22:51:42 +0600

    Swapno search result URL test

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
-
+from flask import Flask,request,Response
+from flask import render_template
+from shwapno import Shwapno
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World! <a href="/search_products">Search</a>'
 
+
+@app.route('/search_products')
+def search_products_func():
+   return render_template('search_products.html')
+
+@app.route('/find_products')
+def find_products_func():
+    pname = request.args.get('pname')
+    swapno_app = Shwapno(pname, 'Uttara')
+    list = swapno_app.prod_list
+    return render_template('results.html',result = list)
 
 
 
