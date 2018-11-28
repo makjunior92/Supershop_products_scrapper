47236a3f32f92e9f10a957c60a3f652ff3208b9d
makjunior92 <makjunior92@gmail.com>
2018-11-29 02:59:42 +0600

    Combined Chaldal and Swapno

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask,request,Response
 from flask import render_template
 from shwapno import Shwapno
+from chaldal import Chaldal
 from flask_cors import CORS
+import json
 app = Flask(__name__)
 
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
@@ -21,6 +23,20 @@ def find_products_func():
     swapno_app = Shwapno(pname, 'Uttara')
     list = swapno_app.prod_list
     return render_template('results.html',result = list)
+
+@app.route('/api/products/search/<key>', methods=['GET'])
+def api_all(key):
+    pname = key
+    swapno_app = Shwapno(pname, 'Uttara')
+    chaldal_app = Chaldal(pname,'Uttara')
+    list = swapno_app.prod_list
+    list2 = chaldal_app.prod_list
+    res = list+list2
+    arr = [{"name": p.name, "shop": p.shop,
+                     "price": p.price, "link": p.link,"img":p.img}
+                    for p in res]
+    #print(json.dumps({"data": arr}, indent=3))
+    return Response(json.dumps(arr, indent=3),  mimetype='application/json')
 
 
 
